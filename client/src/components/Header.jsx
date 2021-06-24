636771629bd5fe2424870137c644c17c5e1f066c
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-06-24 15:50:14 -0500

    minor css styling with flex

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header(props) {
   const { currentUser, handleLogout } = props;
   return (
-    <header>
-      <h1>FitHub</h1>
-      <nav>
-        <Link to="/studios">Studios</Link>
-        {currentUser ? (
-          <>
-            <p>Hello, {currentUser.first_name}</p>
-            <button onClick={handleLogout}>Sign Out</button>
-          </>
-        ) : (
-          <Link to="/signin">Sign In</Link>
-        )}
-      </nav>
+    <div>
+      <header>
+        <h1>FitHub</h1>
+        <nav>
+          <Link to="/studios">Studios</Link>
+          {currentUser ? (
+            <>
+              <p>Hello, {currentUser.first_name}</p>
+              <button onClick={handleLogout}>Sign Out</button>
+            </>
+          ) : (
+            <Link to="/signin">Sign In</Link>
+          )}
+        </nav>
+      </header>
       <hr />
-    </header>
+    </div>
   );
 }
 
