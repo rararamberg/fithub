ec8bdf0208fa6b651c7b6de134d988946c657456
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-06-25 14:23:53 -0500

    conditional for production and development baseurl

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 // set baseurl for development on backend
 // conditional if development use localhost if production use heroku
-const baseUrl = "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://fithub-app-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 // set instance of axios
 const api = axios.create({
