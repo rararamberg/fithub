c86f6e5085273586899e9d6cace729af18cde598
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-06-24 19:14:00 -0500

    miinimal flex styling for studio detail page

--- client/src/screens/StudioDetail/StudioDetail.jsx
+++ client/src/screens/StudioDetail/StudioDetail.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getOneStudio } from "../../services/studios";
+import "./StudioDetail.css";
 
 function StudioDetail() {
   // set state for studio
@@ -18,10 +19,28 @@ function StudioDetail() {
   }, [id]);
 
   return (
-    <div>
-      <h3>This is studio detail page</h3>
+    <div className="studio-detail-container">
       {/* show studio info including classes */}
       {/* if studio belongs currentuser  then give edit option */}
+      <div className="written-studio-info-container">
+        <h2>{studioItem?.business_name}</h2>
+        <h4>
+          <em>{studioItem?.location}</em>
+        </h4>
+        <p>{studioItem?.blurb}</p>
+        <p>Contact:</p>
+        <p>** get studio's owner email **</p>
+      </div>
+      <div className="weekly-studio-schedule-container">
+        <p>Weekly Class Schedule:</p>
+        {studioItem?.fit_classes.map((fitclass) => (
+          <div className="class-box" key={fitclass.id}>
+            {/* render datetime to just day of week and time */}
+            <p>{fitclass.class_name}</p>
+            <button>BOOK</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
